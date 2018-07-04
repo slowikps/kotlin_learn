16848cabe429e04c54394f8d1bbb26ab7c8a9d8c
slowikps <slowikps@gmail.com>
2018-07-04 21:55:50 +0100

    wip

--- src/main/kotlin/com/slowikps/kotlin_learn/controller/HelloController.kt
+++ src/main/kotlin/com/slowikps/kotlin_learn/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package com.slowikps.kotlin_learn.controller
 
+import io.micrometer.core.annotation.Timed
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,15 +13,19 @@ class HelloController {
     lateinit var self: HelloController
 
     @RequestMapping("/")
+    @Timed
     fun index(): String {
+        println("Index: Before sleep1")
         self.sleep1(100)
+        println("Index: Before sleep2")
         self.sleep2(200)
         return "Greetings from Spring Boot!"
     }
 
     fun sleep1(howLong: Long) {
         TimeUnit.MILLISECONDS.sleep(howLong)
-        sleep2(300)
+        println("Slep1: Before sleep2")
+        self.sleep2(300)
     }
 
     fun sleep2(howLong: Long) {
